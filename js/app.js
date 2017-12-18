a687c28dc4178e3c91c216566f67f1969bfd6793
Michael Weber <webermn15@gmail.com>
2017-12-18 11:16:01 -0600

    added a bunch of comments. started work on consolidating player and alien battle logic functions

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ const gamePlay = {
 		alienShips.hull = Math.floor(Math.random()*(6-3+1)+3);
 		alienShips.firepower = Math.floor(Math.random()*(4-2+1)+2);
 		alienShips.accuracy = (Math.floor(Math.random()*(8-6+1)+6) / 10);
+		//log the current alien ship's stats
 		console.log(alienShips);
 	},
 
@@ -49,21 +50,32 @@ const gamePlay = {
 
 	//player battle logic function
 	playerBattle() {
+		//log the players current stats so they can stay updated
 		console.log('Player hull: '+playerShip.hull+'  |  Player max damage: '+playerShip.firepower+'  |  Player accuracy: '+playerShip.accuracy);
+
+		//variables declared that contain the data for comparison against accuracy and damage vs hull
 		let hitOrMiss = Math.random();
 		let playerDamage = Math.floor(Math.random()*(playerShip.firepower - 3 + 1) + 3);
+
+		//log the damage dealt
 		console.log('You deal: '+playerDamage+' damage.'	);
+
+		//if you miss, alert user and pass to alien damage function
 		alert('You fire at the enemy ship!');
 		if (hitOrMiss > playerShip.accuracy) {
 			alert('Your blasters missed! The alien ship is preparing a counter attack!');
 			this.alienBattle();
 		}
+
+		//if you hit, check for damage against hull remaining. route program flow depending on whether or not damage is greater than remaining hull
 		else {
 			if (playerDamage < alienShips.hull) {
 				alienShips.hull -= playerDamage;
 				alert('You land a direct hit, dealing '+playerDamage+' damage! The alien ship is still operational!');
 				this.alienBattle();
 			}
+
+			//if there are no ships remaining, end game, if there are, prompt player to retreat
 			else if (playerDamage >= alienShips.hull) {
 				this.shipsRemaining--;
 				alert('Your firepower is too much for the alien cruiser! You deal '+playerDamage+' damage to the enemy. It\'s hull crumbles under your laser fire, destroying it!');
@@ -78,7 +90,7 @@ const gamePlay = {
 		}
 	},
 
-	//alien battle logic function
+	//alien battle logic function - nearly identical to player battle function. needs a rewrite to consolidate
 	alienBattle() {
 		let hitOrMiss = Math.random();
 		let alienDamage = Math.floor(Math.random()*(alienShips.firepower - 2 + 1) + 2);
@@ -127,7 +139,7 @@ const gamePlay = {
 
 
 
-
+/******************** I WANT TO CONSOLIDATE THE ALIEN AND PLAYER DAMAGE FUNCTIONS ***********************/
 
 
 
